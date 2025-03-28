f68095444de72d92a4bc3676bd1edce7ca03344a
alessandro-sartini <ale.sartini.98@gmail.com>
2025-03-28 16:30:26 +0100

    ADD: DB connection

--- backend/data/db.js
+++ backend/data/db.js
@@ -3,10 +3,10 @@
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'rootroot',
-  database: 'blog_db',
+  host: process.env.DB_HOST,
+  user:process.env.DB_USER ,
+  password: process.env.DB_PSW,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
