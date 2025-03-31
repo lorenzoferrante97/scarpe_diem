0699588c5101b9ac4d4b1981b2d48aec672be2fa
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-31 12:41:13 +0200

    add: hero carousel

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,12 @@
+import HeroCarousel from './HeroCarousel';
+
 export default function Home() {
   return (
     <main>
       <section id="hero-section" className="hero">
-        <div id="hero-carousel" className="hero-slider"></div>
+        <HeroCarousel autoSlideInterval={7000} />
+        {/* hero coupon */}
+        <div></div>
       </section>
     </main>
   );
