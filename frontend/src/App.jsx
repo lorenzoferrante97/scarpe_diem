a7ed8b9fd0905bc71fdf7d92c00b3b90bc273e00
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-31 14:19:41 +0200

    edit: better file organization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { GlobalProvider } from './contexts/GlobalContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // router
 import DefaultLayout from './layouts/DefaultLayout'; // layouts
-import Home from './components/Home'; // pages
+import HomePage from './pages/Homepage';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route Component={DefaultLayout}>
-            <Route path="/" Component={Home}></Route>
+            <Route path="/" Component={HomePage}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,22 @@
-export default function Homepage() {
+import HeroCarousel from '../components/HeroCarousel/HeroCarousel';
+
+export default function HomePage() {
   return (
-    <>
-      <h1 className="font-h1">Titolo Homepage</h1>
-    </>
+    <main>
+      <section id="hero-section" className="hero">
+        <HeroCarousel autoSlideInterval={7000} />
+        {/* hero coupon */}
+        <div className="hero-discount-box">
+          <div className="info">
+            <p className="text-big">Coupon di Primavera!</p>
+            <div className="discount-code">
+              <p className="code">Sconto-10</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* i più venduti */}
+      <section id="home-mostselled"></section>
+    </main>
   );
 }
