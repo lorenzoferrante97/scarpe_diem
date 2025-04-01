96062f7b5b1682226956ba84196a6e4ee97012af
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-04-01 15:22:16 +0200

    END brench

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,20 @@ import { GlobalProvider } from "./contexts/GlobalContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // router
 import DefaultLayout from "./layouts/DefaultLayout"; // layouts
 import Homepage from "./pages/Homepage"; // pages
-import ProductPage from "./pages/ProductPage";
+import ProductPage from "./pages/ProductPage"; //product
 
 function App() {
   return (
-    // <GlobalProvider>
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route Component={DefaultLayout}>
-    //         <Route path="/" Component={Homepage}></Route>
-    //       </Route>
-    //     </Routes>
-    //   </BrowserRouter>
-    // </GlobalProvider>
-    <ProductPage/>
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={Homepage}></Route>
+            <Route path="/slug" Component={ProductPage}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
 
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,31 +1,56 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export default function ProductPage() {
+
+const [product, setProduct] = useState(null)
+
+const {slug} = useParams()
+
+useEffect( () => {
+    axios.get(`http://localhost:3000/products/${slug}`)
+    .then((res) => setProduct(res.data) ) 
+    .catch( (error) => console.error ("errore nel recupero del prodotto", error))
+}, [slug]) 
+
     return (
       <main>
         {/* Sezione prodotto */}
         <section className="product-section">
           <figure className="product-image">
             <img
-              src="https://www.tennisnuts.com/images/product/full/Nike-LunarGlide-6-Womens-Running-Shoe-654434_603_A_PREM.jpg"
-              alt="Air Max 07"
+              src= {product.image}
+              alt= {product.name}
             />
           </figure>
   
           <div className="product-info">
             <div className="product-details">
-              <span className="badge badge-text">Basket</span>
-              <h1 className="h1">Air Max 07</h1>
+
+                {/* questa parte non è corretta come logica perchè la category va cercata nella tabella "category" */}
+                
+              <span className="badge badge-text"> {product.category} </span>
+              <h1 className="h1"> {product.name} </h1>
               <p>
-                Le Nike Air Max 07 combinano stile e comfort in un design iconico. Con una tomaia in mesh traspirante e una suola con l'innovativa tecnologia Air Max, queste scarpe offrono una calzata leggera e un'ammortizzazione superiore per tutta la giornata. Perfette per l'uso quotidiano o per un look sportivo, le Air Max 07 si adattano facilmente a qualsiasi outfit, garantendo un tocco di eleganza e performance. Disponibili in una varietà di colori, queste scarpe sono ideali per chi cerca stile e comfort in un unico prodotto.
+                {product.description} 
               </p>
-              <p className="product-price">€ 129.00</p>
+              <p className="product-price">
+                {product.price}
+              </p>
             </div>
   
             <div className="product-actions-box">
               <label className="text-big" htmlFor="size">
                 Taglia
               </label>
+
+              {/* questa parte non è corretta come logica perchè la size va cercata nella tabella "product_size" */}
+
               <select id="size">
                 <option>Seleziona una taglia</option>
+                    {product.size.map((size) => (
+                <option key={size}> {size} </option>
+              ))}
               </select>
   
               {/* mancano le icone */}
