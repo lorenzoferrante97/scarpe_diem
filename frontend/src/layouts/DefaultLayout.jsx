2d0452baa36dfff1e03c870921fa0e1bbfbfaff0
Giulio <giuliopanza2000@gmail.com>
2025-04-03 10:50:25 +0200

    ADD footer

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function DefaultLayout() {
   return (
@@ -7,6 +8,7 @@ export default function DefaultLayout() {
       <section>
         <Header />
         <Outlet />
+        <Footer />
       </section>
     </>
   );
