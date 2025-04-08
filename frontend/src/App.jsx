5cbff769b89690eb9577cef3eccd2733f5cd3b4b
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-04-08 16:38:30 +0200

    add: search style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import NotFound404 from './pages/notFound404';
 import ScrollTop from './components/ScrollTop';
 import Wishlist from './components/Wishlist';
 import OrderCompleted from './pages/OrderCompleted';
+import CatalogPage from './pages/CatalogPage';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route Component={DefaultLayout}>
             <Route path="/" Component={Homepage}></Route>
+            <Route path="/catalog" Component={CatalogPage}></Route>
             <Route path="/product/:slug" Component={ProductPage}></Route>
             <Route path="/cart" Component={Cart}></Route>
             <Route path="/search" Component={SearchPage}></Route>
