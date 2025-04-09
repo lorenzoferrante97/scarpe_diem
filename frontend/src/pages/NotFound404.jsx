a050942e96a1285591949d8e6f53bc3b829ac7cc
alessandro-sartini <ale.sartini.98@gmail.com>
2025-04-09 15:40:04 +0200

    ADD:Link in 404

--- frontend/src/pages/NotFound404.jsx
+++ frontend/src/pages/NotFound404.jsx
@@ -1,4 +1,5 @@
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { Link } from 'react-router-dom';
 
 export default function NotFound404() {
   return (
@@ -6,10 +7,17 @@ export default function NotFound404() {
       <main>
         <section className="notfound-section">
           <div className="animation-404">
-            <DotLottieReact src="https://lottie.host/f38ea552-20bd-4331-afc0-065c97b46fe1/Ex55FYN84h.lottie" loop autoplay />
+            <DotLottieReact
+              src="https://lottie.host/f38ea552-20bd-4331-afc0-065c97b46fe1/Ex55FYN84h.lottie"
+              loop
+              autoplay
+            />
           </div>
           {/* <span className="notfound-number">404</span> */}
-          <p>Il contenuto che hai cercato non esiste</p>
+          <p className="text-big">Ops abbiamo perso una scarpa, prova a cercarne un'altra</p>
+          <Link to={"/catalog"} className="btn btn-accent">
+           Vai al catalogo
+          </Link>
         </section>
       </main>
     </>
